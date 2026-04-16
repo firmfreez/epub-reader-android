27e24b35a12139b0f2cbf9f522535b10e04ed347
Леон Алексанянц (FirmFreez) <firmfreez@mail.com>
2026-04-16 15:34:03 +0300

    - Доработал обработку ошибок

--- common/data/src/main/java/com/firmfreez/app/common/data/errors/ErrorHandlerImpl.kt
+++ common/data/src/main/java/com/firmfreez/app/common/data/errors/ErrorHandlerImpl.kt
@@ -1,13 +1,13 @@
 package com.firmfreez.app.common.data.errors
 
-import com.firmfreez.app.common.domain.models.result_wrapper.ErrorHandler
 import com.firmfreez.app.common.domain.models.errors.AppThrowable
+import com.firmfreez.app.common.domain.models.result_wrapper.ErrorHandler
 import org.koin.core.annotation.Single
 
 @Single(binds = [ErrorHandler::class])
 class ErrorHandlerImpl() : ErrorHandler {
 
     override fun handleError(throwable: Throwable): AppThrowable {
-        return AppThrowable.unknown()
+        return AppThrowable.custom(throwable)
     }
 }
